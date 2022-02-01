f7925408a0836a4c98c7ba557ab8448000583402
cezenesn <cezene.neves@snlv.com.br>
2022-02-01 14:55:11 -0300

    adding the code

---
+++ README.md
@@ -0,0 +1,1 @@
+Este repositório contém um projeto feito puramente com HTML, CSS, Javascript e bastante dedicação. O mesmo consome uma API que contém uma lista de estabelecimentos e seus respectivos descontos e retorna uma série de pequenos cards contendo nome, porcentagem de desconto e foto do estabelecimento.
---
+++ script.js
@@ -0,0 +1,50 @@
+const shopList = document.getElementById("shopList");
+const searchBar = document.getElementById("searchBar");
+let shopItems = [];
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredShops = shopItems.filter((shop) => {
+    return shop.fantasyName.toLowerCase().includes(searchString);
+  });
+  displayData(filteredShops);
+});
+
+const loadData = async () => {
+  try {
+    const res = await fetch(
+      "https://gdp-prd-clube.s3.amazonaws.com/api/repository/partners/all.json"
+    );
+    shopItems = await res.json();
+    const sortedItems = shopItems.sort(function (a, b) {
+      if (a.fantasyName < b.fantasyName) {
+        return -1;
+      }
+      if (a.fantasyName > b.fantasyName) {
+        return 1;
+      }
+      return 0;
+    });
+    displayData(sortedItems);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayData = (shops) => {
+  const htmlString = shops
+    .map((shop) => {
+      return `
+            <li class="shop">
+                <h2>${shop.fantasyName}</h2>
+                <p>Desconto: ${shop.discountAmount}%</p>
+                <img src="https://clube-static.clubegazetadopovo.com.br/${shop.logo}"></img>
+            </li>
+        `;
+    })
+    .join("");
+  shopList.innerHTML = htmlString;
+};
+
+loadData();
